a7e82f54d8f40ec34594280be66386ea67f7d8de
Lekan Koku <kokulekan23@gmail.com>
2019-05-08 21:31:28 +0200

    removed redux logger dependency

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+
 import reducer from './reducer';
 
-const store = createStore(reducer, {}, applyMiddleware(thunk, logger));
+const store = createStore(reducer, {}, applyMiddleware(thunk));
 export default store;
